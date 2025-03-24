396d127f75e0a968b402bbbf4b1d568f7b4812d7
tdimnet <thomas.dimnet@gmail.com>
2025-03-24 20:59:55 +0100

    Retrieving set of md file and cleaning it

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 import requests
+import re
+
 
 BASE_URL = "https://raw.githubusercontent.com/nx-academy/nx-academy.github.io/refs/heads/main/src/pages/cours"
+
+
+def clean_md_for_rag(content):
+    # Removing YAML's Front Matter
+    content = re.sub(r"^---.*?---\s*", "", content, flags=re.DOTALL)
+
+    # Removing HTML tags (article, br, img, etc.)
+    content = re.sub(r"<(br|/?article|img[^>]*)>", "", content)
+
+    # Removing Markdown images
+    content = re.sub(r"!\[.*?\]\(.*?\)", "", content)
+
+    # Removing multiple empty lines
+    content = re.sub(r"\n{2,}", "\n\n", content)
+
+    # Removing trailing spaces
+    content = re.sub(r"[ \t]+$", "", content, flags=re.MULTILINE)
+
+    return content.strip()
 
 
 def main():
@@ -9,13 +30,12 @@ def main():
     response = requests.get(sample_url)
     if response.status_code == 200:
         with open("decouverte-docker.md", "w", encoding="utf-8") as file:
-            file.write(response.text)
+            file.write(clean_md_for_rag(response.text))
             
         print(f"✅ File saved and downloaded as : {"decouverte-docker"}")
         
     else:
         print(f"❌ Error when downloading the file ({response.status_code}) : {sample_url}")
-    
     
 
 if __name__ == "__main__":
