ed4e33f205ebcfb36e19c3eb0222b0587d4110aa
tdimnet <thomas.dimnet@gmail.com>
2025-04-19 19:18:20 +0200

    Trying to send message with the bot.

--- nx_ai/bot_test.py
+++ nx_ai/bot_test.py
@@ -9,11 +9,21 @@ def run_bot():
     client = discord.Client(intents=intents)
 
     DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+    DISCORD_TEST_CHANNEL = os.environ.get("DISCORD_TEST_CHANNEL")
 
 
     @client.event
     async def on_ready():
         print(f"Bot connected as {client.user}")
+
+        channel_id = 1357783834208243867
+        channel = client.get_channel(channel_id)
+
+        if channel:
+            await channel.send("Hello World depuis le bot")
+            print("Message has been successfully sent.")
+        else:
+            print("Something went wrong when sending the message.")
 
 
     client.run(DISCORD_TOKEN)
