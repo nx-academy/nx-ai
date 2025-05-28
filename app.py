817a24d5e72fbd71b0e4633c1bf4a0d4791f4e7a
tdimnet <thomas.dimnet@gmail.com>
2025-05-28 09:45:37 +0200

    Update app.py by reordering funcs def

--- app.py
+++ app.py
@@ -16,86 +16,23 @@ def cli():
 
 
 @cli.command()
-@click.option("--url", prompt="GitHub URL", help="Enter the full GitHub URL you'd like to fetch. Must be a raw format.")
-@click.option("--name", prompt="Document name", help="The name of the document you'd like to create locally")
-def scrape_github(url, name):
-    """Retrieve a raw file from GitHub, clean it, and store it locally in the folder of your choice."""
-    write_content_from_github(url, name)
-    
-    
-@cli.command()
-@click.option("--location", prompt="File location", help="Enter the path where the file you want to embed is located")
-@click.option("--name", prompt="Document name", help="Enter the name of the document which will be used as metadata for ChromaDB")
-@click.option("--title", prompt="Title of the document", default="", help="Optional / Use it if you want to add a title to store in db")
-@click.option("--author", prompt="Author of the document", default="", help="Optional / Use it if you want to add a document author to store in db")
-@click.option("--url", prompt="URL of the document", default="", help="Optional / Use it if you want to store the URL where the document comes from, e.g. an online blog")
-def create_document(location, name, title, author, url):
-    """Read a local file, usually a md file, and store it as an embedded document with ChromaDB and GPT API."""
-    create_document_with_chroma(location, name, title=title, author=author, url=url)
-
-
-# @cli.command()
-# @click.option("--name", prompt="Document name", help="Enter the document name you want to generate a quiz from")
-# def generate_quiz(name):
-#     """Retrieve a document stored in Chroma, then ask GPT to create a quiz, and store it as a json file once it's done."""
-    # generate_quiz_from_gpt(name)
-
-
-@cli.command()
-@click.option("--name", prompt="Document name", help="Enter the name of the document you want to create a Pull Request on GitHub from.")
-def create_pull_request(name):
-    """Retrieve the generated quiz json file, connect to NX GitHub org, commit the file, and create a PR on main"""
-    create_pull_request_on_github(name)
-
-
-@cli.command()
-def run_discord_bot():
-    run_bot()
-
-
-@cli.command()
-def scrape_article():
-    articles = [
-        {
-            "url": "https://nx.academy/articles/profils-ia-developpeur/",
-            "filename": "01-nx.txt"
-        },
-        {
-            "url": "https://karlgroves.com/ai-is-the-future-of-accessibility/",
-            "filename": "02-karlgroves.txt"
-        },
-        {
-            "url": "https://www.cnet.com/tech/us-wants-judge-to-break-up-google-force-sale-of-chrome-heres-what-to-know/",
-            "filename": "03-cnet.txt"
-        },
-    ]
-    
-    for article in articles:
-        scrape_article_from_internet(article["url"], article["filename"])
-
-
-@cli.command()
-@click.option("--name", prompt="Document name", help="Enter the document name you want to generate a summary from")
-def generate_summary(name):
-    generate_summary_with_gpt(name)
-
-
-@cli.command()
 def generate_quiz_beta():
+    """Generate a quiz with GPT from a raw markdown file and PR it to Astro repo"""
+    # For now, I pass the url and the file name in raw data. I'll see later how to retrieve it from a file, command, and/or discord
     article_url = "https://raw.githubusercontent.com/nx-academy/nx-academy.github.io/refs/heads/main/src/pages/drafts/presentation-registry-docker.md"
     article_name = "presentation-registry-docker.md"
     
     # Retrieve the content from GitHub by scrapping it and store them locally
-    # write_content_from_github(article_url, article_name)
+    write_content_from_github(article_url, article_name)
     
     # Create the doc
-    # create_document_with_chroma(
-    #     f"nx_ai/courses_data/{article_name}",
-    #     article_name[:-3]
-    # )
+    create_document_with_chroma(
+        f"nx_ai/courses_data/{article_name}",
+        article_name[:-3]
+    )
     
     # Generate the quiz
-    # generate_quiz_from_gpt(article_name[:-3])
+    generate_quiz_from_gpt(article_name[:-3])
     
     # PR it to GitHub
     print("Ready to PR it!")
@@ -103,7 +40,8 @@ def generate_quiz_beta():
 
 
 @cli.command()
-def generate_recap():
+def generate_recap_beta():
+    """Generate a summary of articles with GPT from a list of links, create a md friendly Astro file, and PR it to NX Astro repo"""
     # For now, I pass the list of articles here but I should be able to write it from a file.
     articles = [
         {
@@ -186,6 +124,72 @@ def generate_recap():
     
     # PR the summary on GitHub
     print("Now ready to open a PR and enjoy life!")
+
+
+@cli.command()
+@click.option("--url", prompt="GitHub URL", help="Enter the full GitHub URL you'd like to fetch. Must be a raw format.")
+@click.option("--name", prompt="Document name", help="The name of the document you'd like to create locally")
+def scrape_github(url, name):
+    """Retrieve a raw file from GitHub, clean it, and store it locally in the folder of your choice."""
+    write_content_from_github(url, name)
+    
+    
+@cli.command()
+@click.option("--location", prompt="File location", help="Enter the path where the file you want to embed is located")
+@click.option("--name", prompt="Document name", help="Enter the name of the document which will be used as metadata for ChromaDB")
+@click.option("--title", prompt="Title of the document", default="", help="Optional / Use it if you want to add a title to store in db")
+@click.option("--author", prompt="Author of the document", default="", help="Optional / Use it if you want to add a document author to store in db")
+@click.option("--url", prompt="URL of the document", default="", help="Optional / Use it if you want to store the URL where the document comes from, e.g. an online blog")
+def create_document(location, name, title, author, url):
+    """Read a local file, usually a md file, and store it as an embedded document with ChromaDB and GPT API."""
+    create_document_with_chroma(location, name, title=title, author=author, url=url)
+
+
+# @TODO: I let that here for now, I'll probably create a subcommand to handle it later if I need it.
+# @cli.command()
+# @click.option("--name", prompt="Document name", help="Enter the document name you want to generate a quiz from")
+# def generate_quiz(name):
+#     """Retrieve a document stored in Chroma, then ask GPT to create a quiz, and store it as a json file once it's done."""
+    # generate_quiz_from_gpt(name)
+
+
+@cli.command()
+@click.option("--name", prompt="Document name", help="Enter the name of the document you want to create a Pull Request on GitHub from.")
+def create_pull_request(name):
+    """Retrieve the generated quiz json file, connect to NX GitHub org, commit the file, and create a PR on main"""
+    create_pull_request_on_github(name)
+
+
+@cli.command()
+def run_discord_bot():
+    run_bot()
+
+
+@cli.command()
+def scrape_article():
+    articles = [
+        {
+            "url": "https://nx.academy/articles/profils-ia-developpeur/",
+            "filename": "01-nx.txt"
+        },
+        {
+            "url": "https://karlgroves.com/ai-is-the-future-of-accessibility/",
+            "filename": "02-karlgroves.txt"
+        },
+        {
+            "url": "https://www.cnet.com/tech/us-wants-judge-to-break-up-google-force-sale-of-chrome-heres-what-to-know/",
+            "filename": "03-cnet.txt"
+        },
+    ]
+    
+    for article in articles:
+        scrape_article_from_internet(article["url"], article["filename"])
+
+
+@cli.command()
+@click.option("--name", prompt="Document name", help="Enter the document name you want to generate a summary from")
+def generate_summary(name):
+    generate_summary_with_gpt(name)
 
 
 if __name__ == "__main__":
