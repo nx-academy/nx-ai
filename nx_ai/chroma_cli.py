94dad052e8189b569dc86a7ff3ba98b945cb890f
tdimnet <thomas.dimnet@gmail.com>
2025-06-06 16:40:55 +0200

    Create a func to create the chunk (then a doc <- set of chunks)

--- nx_ai/chroma_cli.py
+++ nx_ai/chroma_cli.py
@@ -1,5 +1,7 @@
 import click
 
+from nx_ai.openai import configure_engine
+
 
 @click.group()
 def chroma_group():
@@ -10,11 +12,21 @@ def chroma_group():
 @chroma_group.command()
 def list_documents():
     """List of the documents stored locally"""
-    print("List of the document")
+    engine = configure_engine()
+    db = engine["db"]
+    
+    documents = db.get()
+    for i, doc_id in enumerate(documents["ids"]):
+        print(f"{i + 1}. ID: {doc_id} - Metadata: {documents["metadatas"][i]}")
 
 
 @chroma_group.command()
-@click.argument("doc_id")
-def read_doc(doc_id):
-    """Retrieve if exists the doc with the id ${doc_id}"""
-    print(f"Getting the doc_id: {doc_id}")
+@click.argument("id")
+def read_chunk(id):
+    """Retrieve if exists a chunk with the id ${id}"""
+    engine = configure_engine()
+    db = engine["db"]
+    
+    chunk = db.get(ids=[id])
+    print(f"Chunk: {chunk['documents'][0]}")
+    print(f"Metadata: {chunk['metadatas'][0]}")
