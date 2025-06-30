213cbb6e76dcc78b74cd0237582d952408e5ccec
tdimnet <thomas.dimnet@gmail.com>
2025-06-30 19:20:28 +0200

    Making a web search on NX and get a summary (and it works!)

--- nx_ai/openai_cli.py
+++ nx_ai/openai_cli.py
@@ -23,3 +23,20 @@ def say_hello():
     )
     
     print(response.output[0].content[0].text)
+
+
+@openai_group.command()
+def summarize_article():
+    """Send a url to GPT and ask it to summarize it in 3 lines"""
+    print("Sendin the request to GPT")
+    response = client.responses.create(
+        model=MODEL,
+        tools=[{
+            "type": "web_search_preview"
+        }],
+        input="Peux-tu me résumer cet article en français en 3 lignes claires et synthétiques comme une fiche de veille pour développeurs ? https://nx.academy/fiches/presentation-registry-docker/"
+    )
+    
+    print("====")
+    print(response.output[1].content[0].text)
+    print("====")
