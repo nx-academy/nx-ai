3b3176789f6b69c35693610da6188c8eb6e62c1a
tdimnet <thomas.dimnet@gmail.com>
2025-07-08 17:31:34 +0200

    Delete useless files (and soon useless libs)

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from nx_ai.openai_service.openai_cli import openai_group
 from nx_ai.github_service.github_cli import github_group
 from nx_ai.workflows.workflows_cli import workflows_group
 from nx_ai.discord_service.discord_cli import discord_group
-from nx_ai.scraper_service.scraper_cli import scraper_group
 
 
 @click.group()
@@ -15,7 +14,6 @@ def cli():
 cli.add_command(discord_group, name="discord")
 cli.add_command(openai_group, name="openai")
 cli.add_command(github_group, name="github")
-cli.add_command(scraper_group, name="scraper")
 cli.add_command(workflows_group, name="workflows")
 
 
--- nx_ai/scraper_service/scrape_website.py
+++
@@ -1,9 +0,0 @@
-import requests
-from readability import Document
-from bs4 import BeautifulSoup
-
-
-def scrape_website(url: str):
-    print("====")
-    print(url)
-    print("====")
