219462e11d4d7e517fa2362357d75d1b53893be2
tdimnet <thomas.dimnet@gmail.com>
2025-07-08 14:00:47 +0200

    Finish writing test suites

--- tests/test_gpt_generated_quiz.py
+++ tests/test_gpt_generated_quiz.py
@@ -8,8 +8,9 @@ from nx_ai.openai_service.gpt_models import GPTGeneratedQuiz, FakeResponse
 
 
 def test_gpt_generated_quiz():
-    print("====")
-    print("====")
-    print("====")
-    
-    assert True == True
+    with open("mock/generated_quiz.json") as f:
+        mock = json.load(f)
+        gpt_generated_quiz = GPTGeneratedQuiz(FakeResponse(json.dumps(mock), use_tool=True))
+        
+        assert gpt_generated_quiz.data[0]["question"] == "Qu'est-ce qu'un registry Docker ?"
+        assert gpt_generated_quiz.data[1]["answer"] == "Docker Hub"
