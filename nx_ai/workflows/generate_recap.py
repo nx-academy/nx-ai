0721feda923a9126ed6e0bddc127f37212f5cdf9
tdimnet <thomas.dimnet@gmail.com>
2025-08-18 14:43:40 +0200

    Now writing recap (works with simulated data)

--- nx_ai/workflows/generate_recap.py
+++ nx_ai/workflows/generate_recap.py
@@ -1,5 +1,6 @@
 import os
 
+
 from nx_ai.openai_service.openai_api import summarize_article_with_gpt
 
 def create_recap_folder():
@@ -14,11 +15,41 @@ def generate_recap_beta(urls: list[str], simulate: bool):
         article = summarize_article_with_gpt(url=url, simulate=simulate)
         articles.append(article)
         
+    recap_text = """---
+layout: ../../layouts/BlogPostLayout.astro
+
+title: "METTRE A JOUR | Le récap #3 - Juin 2025"
+description: METTRE A JOUR
+
+imgAlt: Un vendeur de journaux dans un kiosque parisien, pixel art
+imgSrc: /images/articles/kiosque-journaux.webp
+
+kind: Articles
+author: Thomas
+draft: false
+publishedDate: METTRE A JOUR | 06/27/2025
+---
+    
+# METTRE A JOUR | Le récap #3 - Juin 2025
+
+<img src="/images/articles/kiosque-journaux.webp" alt="Un vendeur de journaux dans un kiosque parisien, pixel art" style="aspect-ratio: 1792 / 1024; object-fit: cover; width: 100%; display: block; object-position: top" />
+
+<br>
+"""
+      
+    for article in articles:
+        recap_text += f"""## {article.data["title_fr"]}
+<small>{article.data["author"]}</small>
+
+{article.data["article_summary"]}
+
+[Lire l'article](#)
+
+<br>
         
-    print("====")
-    print(articles)
-    print("====")
-    
+---
+"""
+
     create_recap_folder()
-    
-    
+    with open(f"nx_ai/recap_data/test.md", "w", encoding="utf-8") as file:
+        file.write(recap_text)
