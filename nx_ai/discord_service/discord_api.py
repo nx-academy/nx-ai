d229fd71ccda5d8107f1557c6e7b209efcc3e157
tdimnet <thomas.dimnet@gmail.com>
2025-08-20 19:39:39 +0200

    Start to cpde the Back office for the News Feed

--- nx_ai/discord_service/discord_api.py
+++ nx_ai/discord_service/discord_api.py
@@ -1,30 +1,36 @@
 import discord
+from discord import app_commands
+from discord.ui import Modal, TextInput
 import os
 
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+GUILD_ID = 1357783834208243864
 DISCORD_RECAP_CHANNEL = int(os.environ.get("DISCORD_RECAP_CHANNEL"))
 
 
+class NewsFeedClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.tree = app_commands.CommandTree(self)
+    
+    async def setup_hook(self):
+        guild = discord.Object(id=GUILD_ID)
+        self.tree.copy_global_to(guild=guild)
+        await self.tree.sync(guild=guild)
+
+
+client = NewsFeedClient()
+
 def run_discord_bot():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    
-    client = discord.Client(intents=intents)
-    
     @client.event
     async def on_ready():
         print(f"Bot connected as {client.user}")
-        
-        channel_id = DISCORD_RECAP_CHANNEL
-        channel = client.get_channel(channel_id)
-        
-        if not channel:
-            print("Unabled to connect to the channel")
-            await client.close()
-            return
     
-        
+    @client.tree.command(name="add_news", description="Créer une nouvelle news")
+    async def create_news(interaction: discord.Interaction):
+        print("====")
+        print("====")
+        print("====")
+    
     client.run(DISCORD_TOKEN)
-    
-    
