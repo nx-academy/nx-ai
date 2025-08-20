256030c8f2944770e2c014b614d7f1c084eae155
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-08-20 20:15:16 +0200

    (Feat) Create Discord Modal for creating news (#63)
    * Start to cpde the Back office for the News Feed
    
    * Now able to open a modal and create a news
    
    * Update message

--- nx_ai/discord_service/discord_api.py
+++ nx_ai/discord_service/discord_api.py
@@ -1,30 +1,51 @@
 import discord
+from discord import app_commands
+from discord.ui import Modal, TextInput
 import os
 
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+GUILD_ID = 1357783834208243864
 DISCORD_RECAP_CHANNEL = int(os.environ.get("DISCORD_RECAP_CHANNEL"))
 
 
+class NewsModal(Modal, title="Créer une nouvelle news"):
+    title_input = TextInput(label="Titre", placeholder="Titre de la news", max_length=255)
+    content_input = TextInput(
+        label="Contenu",
+        style=discord.TextStyle.paragraph,
+        placeholder="Contenu de la news",
+        max_length=500)
+    url_input = TextInput(label="URL", placeholder="https://...")
+    
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            f"✨ Proposition reçue !\n\n**Titre**: {self.title_input}\n**Résumé**: {self.content_input}\n**URL**: {self.url_input}",
+            ephemeral=True
+        )
+
+
+class NewsFeedClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.tree = app_commands.CommandTree(self)
+    
+    async def setup_hook(self):
+        guild = discord.Object(id=GUILD_ID)
+        self.tree.copy_global_to(guild=guild)
+        await self.tree.sync(guild=guild)
+
+
+client = NewsFeedClient()
+
 def run_discord_bot():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    
-    client = discord.Client(intents=intents)
-    
     @client.event
     async def on_ready():
         print(f"Bot connected as {client.user}")
-        
-        channel_id = DISCORD_RECAP_CHANNEL
-        channel = client.get_channel(channel_id)
-        
-        if not channel:
-            print("Unabled to connect to the channel")
-            await client.close()
-            return
     
-        
+    @client.tree.command(name="add_news", description="Créer une nouvelle news")
+    async def create_news(interaction: discord.Interaction):
+        modal = NewsModal()
+        await interaction.response.send_modal(modal)
+    
     client.run(DISCORD_TOKEN)
-    
-    
