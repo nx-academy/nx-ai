54b7a5959235c8702b5f0a67b307d5fdc66ccb78
tdimnet <thomas.dimnet@gmail.com>
2025-08-25 18:20:55 +0200

    Make CRUD commands for OpenAI Vectors Store

---
+++ nx_ai/vector_store_service/vector_store_api.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+
+
+client = OpenAI()
+
+
+def list_openai_vector_stores():
+    vector_stores = client.vector_stores.list()
+    return vector_stores
+
+
+def create_openai_vector_store(name: str):
+    vector_store = client.vector_stores.create(
+        name=name
+    )
+    return vector_store
+
+
+def get_openai_vector_store(id: str):
+    vector_store = client.vector_stores.retrieve(
+        vector_store_id=id
+    )
+    return vector_store
+
+
+def delete_openai_vector_store(id: str):
+    vector_store = client.vector_stores.delete(
+        vector_store_id=id
+    )
+    return vector_store
