78168221d0ee4845caab75ff32da63e32d0b1b1e
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-09-13 17:18:23 +0200

    Start to implement quiz creation command

--- nx_ai/discord_service/discord_api.py
+++ nx_ai/discord_service/discord_api.py
@@ -14,6 +14,7 @@ GUILD_ID = 1357783834208243864
 DISCORD_BO_NEWS_FEED = 1407779612439613522
 DISCORD_BO_NEWSROOM_IA = 1408853151733518376
 DISCORD_BO_LE_RECAP = 1408121527433433221
+DISCORD_BO_QUIZ = 1416441425238949988
 
 
 class DiscordClient(discord.Client):
@@ -33,6 +34,16 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f"Bot connected as {client.user}")
+
+    @client.tree.command(name="create_quiz", description="Créer un nouveau quiz à partir d'une fiche technique")
+    async def create_quiz(interaction: discord.Interaction):
+        if interaction.channel_id != DISCORD_BO_QUIZ:
+            await interaction.response.send_message(
+                "❌ Cette commande n’est autorisée que dans le channel dédié.",
+            )
+            return
+        
+        await interaction.response.send_message("Ok, let's go!")
         
     @client.tree.command(name="create_recap", description="Créer un nouveau Le Recap")
     async def create_recap(interaction: discord.Interaction):
