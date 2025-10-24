ea785ee126ed3daf1c551503d9d6e9a0a6d19cd5
Thomas Dimnet <thomas.dimnet@gmail.com>
2025-10-24 17:15:10 +0200

    (Feat) Add RAG System for Text (#103)
    * Start to implement the new Discord command: rewrite text with my style
    
    * Make rewrite command runs on discord
    
    * Trying to call GPT API and Rag inside Modal
    
    * Now Summary RAG is working

--- nx_ai/discord_service/discord_api.py
+++ nx_ai/discord_service/discord_api.py
@@ -1,12 +1,15 @@
 import os
 
 import discord
-from discord import app_commands
+from discord import Interaction, app_commands
 
 from nx_ai.discord_service.news_modal import NewsModal
 from nx_ai.discord_service.recap_modal import RecapModal
 from nx_ai.discord_service.quiz_modal import QuizModal
-from nx_ai.openai_service.openai_api import fetch_news_with_gpt_web_search
+from nx_ai.discord_service.summary_modal import SummaryModal
+from nx_ai.openai_service.openai_api import (
+        fetch_news_with_gpt_web_search,
+        rewrite_summary_with_personal_style)
 
 
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
@@ -16,6 +19,7 @@ DISCORD_BO_NEWS_FEED = 1407779612439613522
 DISCORD_BO_NEWSROOM_IA = 1408853151733518376
 DISCORD_BO_LE_RECAP = 1408121527433433221
 DISCORD_BO_QUIZ = 1416441425238949988
+DISCORD_BO_STYLE_TEXT = 1425854954194600017
 
 
 class DiscordClient(discord.Client):
@@ -37,7 +41,7 @@ def run_discord_bot():
         print(f"Bot connected as {client.user}")
 
     @client.tree.command(name="create_quiz", description="Créer un nouveau quiz à partir d'une fiche technique")
-    async def create_quiz(interaction: discord.Interaction):
+    async def create_quiz(interaction: Interaction):
         if interaction.channel_id != DISCORD_BO_QUIZ:
             await interaction.response.send_message(
                 "❌ Cette commande n’est autorisée que dans le channel dédié.",
@@ -47,7 +51,7 @@ def run_discord_bot():
         await interaction.response.send_modal(QuizModal())
         
     @client.tree.command(name="create_recap", description="Créer un nouveau Le Recap")
-    async def create_recap(interaction: discord.Interaction):
+    async def create_recap(interaction: Interaction):
         if interaction.channel_id != DISCORD_BO_LE_RECAP:
             await interaction.response.send_message(
                 "❌ Cette commande n’est autorisée que dans le channel dédié.",
@@ -58,7 +62,7 @@ def run_discord_bot():
         await interaction.response.send_modal(RecapModal())
     
     @client.tree.command(name="add_news", description="Créer une nouvelle news")
-    async def create_news(interaction: discord.Interaction):
+    async def create_news(interaction: Interaction):
         if interaction.channel_id != DISCORD_BO_NEWS_FEED:
             await interaction.response.send_message(
                 "❌ Cette commande n’est autorisée que dans le channel dédié.",
@@ -70,7 +74,7 @@ def run_discord_bot():
         
     @client.tree.command(name="fetch_news", description="Chercher des news sur Internet")
     @app_commands.describe(simulate="Si activé, renvoie des données mockées (simulate=True)")
-    async def fetch_news(interaction: discord.Interaction, simulate: bool = True):
+    async def fetch_news(interaction: Interaction, simulate: bool = True):
         if interaction.channel_id != DISCORD_BO_NEWSROOM_IA:
             await interaction.response.send_message(
                 "❌ Cette commande n’est autorisée que dans le channel dédié.",
@@ -91,6 +95,18 @@ def run_discord_bot():
             )
             await interaction.followup.send(embed=embed)
         await interaction.followup.send("✅ Travail terminé.")
-    
+
+    @client.tree.command(name="style-text",
+                         description="Reécrire un texte avec mon style personnel")
+    async def style_text(interaction: Interaction):
+        if interaction.channel_id != DISCORD_BO_STYLE_TEXT:
+            await interaction.response.send_message(
+                    content="❌ Cette commande n’est autorisée que dans le channel dédié.",
+                    ephemeral=True
+            )
+            return
+
+        await interaction.response.send_modal(SummaryModal())
+
     
     client.run(DISCORD_TOKEN)
